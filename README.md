9afc6854c2d1a09d3040c8ee2ec7906849f89df3
raashidanwar <raashid12anwar@gmail.com>
2024-12-28 23:57:55 +0530

    Cleanup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # raqueue
-Go message queue module
+raqueue is a simple in-memory queue module for go. It stores messages in data/raqueue-<random-string>.txxt files. It is also thread safe i.e. supports multiple readers and writers.
--- raqueue.go
+++ raqueue.go
@@ -5,25 +5,21 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sync"
 	"time"
 )
 
 type RAQueue struct {
-	msgIndex uint   // Tracks the next message index to read
-	totalMsg uint   // Total number of messages in the queue
-	capacity uint   // Maximum capacity of the queue
-	filepath string // Path to the file storing the queue
-	offset   int64  // Tracks the byte offset for consecutive reads
-}
-
-type Option struct {
-	Capacity uint
+	msgIndex uint       // Tracks the next message index to read
+	totalMsg uint       // Total number of messages in the queue
+	filepath string     // Path to the file storing the queue
+	offset   int64      // Tracks the byte offset for consecutive reads
+	m        sync.Mutex // Mutex for concurrent access
 }
 
 var raQueueClient *RAQueue
 
-// New initializes the RAQueue instance
-func New(option *Option) *RAQueue {
+func New() *RAQueue {
 	if raQueueClient == nil {
 		filePath, err := createFile()
 		if err != nil {
@@ -32,7 +28,6 @@ func New(option *Option) *RAQueue {
 		raQueueClient = &RAQueue{
 			msgIndex: 0,
 			totalMsg: 0,
-			capacity: max(1, option.Capacity),
 			filepath: filePath,
 			offset:   0,
 		}
@@ -40,22 +35,20 @@ func New(option *Option) *RAQueue {
 	return raQueueClient
 }
 
-// Send appends data to the queue
 func (r *RAQueue) Send(v interface{}) (bool, error) {
-	// Serialize data to JSON
+	r.m.Lock()
+	defer r.m.Unlock()
 	data, err := json.Marshal(v)
 	if err != nil {
 		return false, err
 	}
 
-	// Open file in append mode
 	file, err := os.OpenFile(r.filepath, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		return false, err
 	}
 	defer file.Close()
 
-	// Write data with a newline delimiter
 	if _, err := file.Write(append(data, '\n')); err != nil {
 		return false, err
 	}
@@ -65,31 +58,27 @@ func (r *RAQueue) Send(v interface{}) (bool, error) {
 	return true, nil
 }
 
-// Read reads the next message from the queue
 func (r *RAQueue) Read(v interface{}) error {
-	// Open file in read mode
+	r.m.Lock()
+	defer r.m.Unlock()
 	file, err := os.Open(r.filepath)
 	if err != nil {
 		return fmt.Errorf("could not open file: %v", err)
 	}
 	defer file.Close()
 
-	// Seek to the last read position
 	if _, err := file.Seek(r.offset, 0); err != nil {
 		return fmt.Errorf("could not seek to offset: %v", err)
 	}
 
-	// Read the next line
 	reader := bufio.NewReader(file)
 	line, err := reader.ReadString('\n')
 	if err != nil {
 		return fmt.Errorf("could not read line: %v", err)
 	}
 
-	// Update the offset
 	r.offset += int64(len(line))
 
-	// Deserialize JSON data
 	if err := json.Unmarshal([]byte(line), v); err != nil {
 		return fmt.Errorf("could not unmarshal data: %v", err)
 	}
@@ -99,12 +88,10 @@ func (r *RAQueue) Read(v interface{}) error {
 	return nil
 }
 
-// logAction logs the actions performed on the queue
 func (r *RAQueue) logAction(action string) {
 	fmt.Printf("[%s] Action: %s, File: %s\n", time.Now().Format(time.RFC3339), action, r.filepath)
 }
 
-// createFile creates a new file for the queue
 func createFile() (string, error) {
 	currentDir, err := os.Getwd()
 	if err != nil {
@@ -125,11 +112,4 @@ func createFile() (string, error) {
 
 	fmt.Println("File created successfully:", filePath)
 	return filePath, nil
-}
-
-func max(a, b uint) uint {
-	if a > b {
-		return a
-	}
-	return b
 }
